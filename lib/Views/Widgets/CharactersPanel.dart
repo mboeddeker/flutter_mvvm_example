292eedc80175fad4e31e5e289f60fec799de6fcf
Marvin Böddeker <marvin.boeddeker@gmail.com>
2019-01-15 19:24:57 +0100

    Added 2 Files

---
+++ lib/Views/Widgets/CharactersPanel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_template/Models/Character.dart';
+import 'package:mvvm_template/ViewModels/MainPageViewModel.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CharactersPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
---
+++ lib/Views/Widgets/PlanetPanel.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_template/Models/Planet.dart';
+import 'package:mvvm_template/ViewModels/MainPageViewModel.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class PlanetsPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
